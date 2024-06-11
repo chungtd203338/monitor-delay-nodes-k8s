c77cca3f675ace8b163bb5dcf48b077155839259
chungtd203338 <chungfaker@gmail.com>
2024-06-11 22:34:13 +0700

    update add label

--- add_label_node.sh
+++ add_label_node.sh
@@ -1,6 +1,19 @@
-kubectl label node master pod-assign=node1
-kubectl label node worker1 pod-assign=node2
-kubectl label node worker2 pod-assign=node3
+#!/bin/bash
+
+# Lấy danh sách các node từ Kubernetes
+nodes=$(kubectl get nodes -o jsonpath='{.items[*].metadata.name}')
+node_array=($nodes)
+
+# Khởi tạo biến đếm
+counter=1
+
+for node in "${node_array[@]}"
+do
+  label="node$counter"
+  echo "Labeling $node with pod-assign=$label"
+  kubectl label nodes $node pod-assign=$label --overwrite
+  counter=$((counter+1))
+done
 
 # check label node
 kubectl get nodes --show-labels=true
