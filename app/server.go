0e911e6b38d1a5e06992d74e933ade2cb66a40a1
chungtd203338 <chungfaker@gmail.com>
2024-06-22 14:55:54 +0700

    fix log pod

--- app/server.go
+++ app/server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"strings"
+	"time"
 
 	// corev1 "k8s.io/api/core/v1"
 	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -38,20 +40,21 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-
+	nodes, _ := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	podLog := make([]*rest.Request, len(nodes.Items))
+	log := make([]string, len(nodes.Items))
+	podName := make([]string, len(nodes.Items))
+	line := int64(2)
+	for i := 1; i <= len(nodes.Items); i++ {
+		podName[i-1] = "pod" + strconv.Itoa(i)
+		podLog[i-1] = clientset.CoreV1().Pods("default").GetLogs(podName[i-1], &corev1.PodLogOptions{
+			TailLines: &line,
+		})
+	}
 	e := echo.New()
 	e.GET("/metrics", func(c echo.Context) error {
-		nodes, _ := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
-		podLog := make([]*rest.Request, len(nodes.Items))
-		log := make([]string, len(nodes.Items))
-
-		podName := make([]string, len(nodes.Items))
-		for i := 1; i <= len(nodes.Items); i++ {
-			podName[i-1] = "pod" + strconv.Itoa(i)
-		}
-
+		start := time.Now()
 		for i := 0; i < len(nodes.Items); i++ {
-			podLog[i] = clientset.CoreV1().Pods("default").GetLogs(podName[i], &corev1.PodLogOptions{})
 			podLogs, err := podLog[i].Stream(context.TODO())
 			if err != nil {
 				panic(err.Error())
@@ -65,10 +68,13 @@ func main() {
 			}
 			str := buf.String()
 			lines := strings.Split(str, "\n")
-			log[i] = strings.Join(lines[len(lines)-len(nodes.Items):], "\n")
+			log[i] = strings.Join(lines, "\n")
 		}
 
 		laslog := strings.Join(log, "")
+		end := time.Now()
+		elapsed := end.Sub(start)
+		fmt.Printf("Thời gian thực hiện: %s\n", elapsed)
 		return c.String(http.StatusOK, laslog)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
